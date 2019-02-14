d15bda4cbb1559e505a93d80317bf05730683275
antonio teran <teran@mit.edu>
2019-02-14 00:58:34 -0500

    add wrapper test, mod cmake

---
+++ tests/testWrapper.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file testWrapper.hpp
+ * @brief test glfw wrapper for encapsulating C API
+ * @date Feb 13, 2019
+ * @author Antonio Teran (teran@mit.edu)
+ */
+
+#include "navviz/GlfwWrapper.hpp"
+#include "navviz/GlfwAppManager.hpp"
+
+int main(void) {
+
+  std::cout << "testing glfw wrappers" << std::endl;
+
+  return (0);
+
+}
+
