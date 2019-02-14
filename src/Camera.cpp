fe7f26833d2f07c676f160812ec4ac2fb8bdd5cc
antonio teran <teran@mit.edu>
2019-02-13 19:05:35 -0500

    add initial source file for navviz

---
+++ src/Camera.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file Camera.cpp
+ * @brief Camera object to move around in OpenGL
+ * @date Feb 13, 2019
+ * @author Antonio Teran (teran@mit.edu)
+ * taken from:
+ * https://learnopengl.com/code_viewer_gh.php?code=includes/learnopengl/camera.h
+ * all credit goes to them!
+ */
+
+#include "navviz/Camera.h"
+
+/* ************************************************************************** */
+Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch) :
+      Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED),
+      MouseSensitivity(SENSITIVITY), Zoom(ZOOM) {
+  Position = position;
+  WorldUp  = up;
+  Yaw      = yaw;
+  Pitch    = pitch;
+  updateCameraVectors();
+}
+
+/* ************************************************************************** */
+Camera::~Camera() {}
+
